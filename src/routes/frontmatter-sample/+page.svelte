747e6ebfd21b8841dd1939b30979d4f5d823430a
evanboerchers <evan.boerchers@gmail.com>
2025-07-25 09:17:38 -0700

    page work

--- src/routes/frontmatter-sample/+page.svelte
+++ src/routes/frontmatter-sample/+page.svelte
@@ -1,3 +1,9 @@
 <script lang='ts'>
-    import {} from  '$lib/content/full-svx-sample.svx'
+    import Content, { metadata } from  '$lib/content/frontmatter-sample.svx'
 </script>
+
+<div class='prose'>
+    <Content/>
+    <hr/>
+    <p><b>Author:</b> {metadata.author}</p>
+</div>  
