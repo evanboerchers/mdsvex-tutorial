65b99c999045de00bd211ec8516a15ceafb9a620
evankboerchers <evan.boerchers@gmail.com>
2025-07-27 01:04:12 -0700

    added .nojekyll to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { writeFileSync } from 'node:fs'
+import type { Plugin } from 'vite'
+
+function addNoJekyll(): Plugin {
+	return {
+	  name: 'add-nojekyll',
+	  closeBundle() {
+		writeFileSync('/build/.nojekyll', '');
+	  }
+	};
+  }
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), addNoJekyll()]
 });
