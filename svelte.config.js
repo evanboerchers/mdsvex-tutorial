d7010bd247ef31fe12e9860fbf2cbeb55c491e65
evankboerchers <evan.boerchers@gmail.com>
2025-07-26 23:46:49 -0700

    config cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,14 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { join, dirname } from 'path';
- import { fileURLToPath } from 'url';
- 
- const __filename = fileURLToPath(import.meta.url);
- const __dirname = dirname(__filename);
- 
- const articleLayout = join(__dirname, './src/lib/layouts/article.svelte');
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess(), mdsvex(
 		{
 			layout: {
-				article: articleLayout
+				article: import.meta.dirname + '/src/lib/layouts/article.svelte'
 			},
 			extensions: ['.md', '.svx'],
 		}
