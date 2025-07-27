bde8df01e29c7f17ec960087a05ccdf70ce59df7
evankboerchers <evan.boerchers@gmail.com>
2025-07-26 23:52:16 -0700

    added static adapter and prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
