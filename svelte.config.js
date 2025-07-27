e45a4eae4b70e2acc337082648257865c645554d
evankboerchers <evan.boerchers@gmail.com>
2025-07-26 23:53:02 -0700

    fixed build

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 
