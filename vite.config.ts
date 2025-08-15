35cbeb31bfeab9b50c33578746af3a48ce530d1a
evanboerchers <evan.boerchers@gmail.com>
2025-08-15 13:54:02 -0700

    fix plugin issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { writeFileSync } from 'node:fs'
+import { writeFileSync, existsSync, mkdirSync } from 'node:fs'
 import type { Plugin } from 'vite'
 
 function addNoJekyll(): Plugin {
 	return {
 	  name: 'add-nojekyll',
 	  closeBundle() {
-		writeFileSync('/build/.nojekyll', '');
+		if (!existsSync('./build')) {
+			mkdirSync('./build')
+		}
+		writeFileSync('./build/.nojekyll', '');
 	  }
 	};
   }
